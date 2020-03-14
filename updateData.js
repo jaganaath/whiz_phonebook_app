36937772d7ff2761f763e3e593fb064cc8e390b8
Jaganaath Jayakumar <jaganaath.j@gmail.com>
2020-03-14 14:11:26 +1100

    Create updateData.js

---
+++ updateData.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region:'ap-southeast-2'});
+
+// Create the DynamoDB service object
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  
+  // Create param
+  const params = {
+    TableName: 'phonebook_example',
+    Key: {
+      contactNumber: data.contactNumber
+    },
+    UpdateExpression: "set contactName=:contactName",
+    ExpressionAttributeValues:{
+        ":contactName": data.contactName
+    },
+    ReturnValues:"ALL_NEW"
+  };
+
+  // write the item to the database
+  ddb.update(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error("Error: ", error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the item.',
+      });
+      return;
+    }
+
+    // create a response with status code and body
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Item Updated'),
+    };
+    callback(null, response);
+  });
+}
